92ea27a6b2440b5e8c40a206d3e9f28a127977c1
manikantayalla-debug <manikantayalla03@gmail.com>
2026-09-15 21:30:53 +0530

    Fixed Prime Numbers Prog

--- Day 3/PrimeNumbers.py
+++ Day 3/PrimeNumbers.py
@@ -1,16 +1,18 @@
-start,end = 2, 50
+start , end = 2 , 50
 count = 0
 temp = start
-while temp!=end:
+
+while temp <=end:
     isPrime = True
-    t = start
-    start+=1
-    i=2
-    while i < t:
-        if t % i == 0:
+    i = 2
+    while i < temp:
+        if temp % i == 0:
             isPrime = False
             break
+        i+=1
+
     if isPrime:
-        count +=1
+        count+=1
     temp +=1
+
 print(count)
